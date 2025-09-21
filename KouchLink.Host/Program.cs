15a28285f1648a26c11d75e093a3ed98a18d0fcc
h4cander <h4cander@gmail.com>
2025-09-21 18:37:55 +0800

    init commit

---
+++ KouchLink.Host/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KouchLink.Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
